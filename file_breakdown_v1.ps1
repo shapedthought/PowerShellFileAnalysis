d4f9784b4921cdbf3b2a702f72eca8695f286f40
Ed Howard <shapedthought@gmail.com>
2019-02-06 21:37:49 +0000

    Updates at last!

--- file_breakdown_v1.ps1
+++ file_breakdown_v1.ps1
@@ -3,17 +3,9 @@
 #Inital scan of file system
 
 $dirLoc = Read-Host -Prompt "Enter the file system location"
-$i = 1
 Write-Host "Pre-scan please wait...."
 
 $files = Get-ChildItem $dirLoc -File -Recurse
-
-#$files | ForEach-Object {
-#    Write-Progress -Id 1 -Activity "Scanning file $($_.name)" -Status "File $i of $($files.Count)" -PercentComplete (($i / $files.Count) * 100) 
-#    $i++
-#}
-
-Write-Progress -id 1 -Activity "File system scan" -Completed
 
 #Total used capacity
 
@@ -24,8 +16,8 @@ Write-Host ""
 
 #Largest file
 
-$largestFile = ($files | Sort length -Descending | Select-Object -first 1).FullName
-$largestFileCap = ($files | Select-Object length | Sort length -Descending | select -first 1).length / 1GB
+$largestFile = ($files | Sort-Object length -Descending | Select-Object -first 1).FullName
+$largestFileCap = ($files | Select-Object length | Sort-Object length -Descending | Select-Object -first 1).length / 1GB
 Write-Host "Largest file is" $largestFile "at" ("{0:N2} GB" -f $largestFileCap)
 Write-Host ""
 
@@ -36,7 +28,7 @@ Write-Host "Average File size is" ("{0:N4} GB" -f $averageFile)
 Write-Host ""
 
 #Longest pathname
-$longPath = $files | Select-Object FullName | Sort length | select -first 1;
+$longPath = $files | Select-Object FullName | Sort-Object length | Select-Object -first 1;
 Write-Host "Longest Path is" $longPath
 Write-Host ""
 
@@ -49,18 +41,18 @@ $date6months = $date.AddMonths(-6)
 
 #Get the file count for Creation Time in each timeframe
 
-$CtotalFiles = $files | Measure-Object | %{$_.Count}
+$CtotalFiles = $files | Measure-Object | ForEach-Object{$_.Count}
 
-$CdateWeek1 = $files | Where-Object {$_.CreationTime -ge $dateWeek } | Measure-Object | %{$_.Count}
+$CdateWeek1 = $files | Where-Object {$_.CreationTime -ge $dateWeek } | Measure-Object | ForEach-Object{$_.Count}
 $CdateWeek1Cap = ($files | Where-Object {$_.CreationTime -ge $dateWeek } | Measure-Object -Sum length).sum /1GB
 
-$CdateMonths = $files | Where-Object {$_.CreationTime -ge $dateMonth -and $_.CreationTime -lt $dateWeek } | Measure-Object | %{$_.Count}
+$CdateMonths = $files | Where-Object {$_.CreationTime -ge $dateMonth -and $_.CreationTime -lt $dateWeek } | Measure-Object | ForEach-Object{$_.Count}
 $CdateMonthsCap = ($files | Where-Object {$_.CreationTime -ge $dateMonth -and $_.CreationTime -lt $dateWeek } | Measure-Object -Sum length).sum / 1GB
 
-$Cdate6Months1 = $files | Where-Object {$_.CreationTime -ge $date6Months -and $_.CreationTime -lt $dateMonth } | Measure-Object | %{$_.Count}
+$Cdate6Months1 = $files | Where-Object {$_.CreationTime -ge $date6Months -and $_.CreationTime -lt $dateMonth } | Measure-Object | ForEach-Object{$_.Count}
 $Cdate6Months1Cap = ($files | Where-Object {$_.CreationTime -ge $date6Months -and $_.CreationTime -lt $dateMonth } | Measure-Object -Sum length).sum / 1GB
 
-$Cdate6MonthsPlus = $files | Where-Object {$_.CreationTime -lt $date6Months } | Measure-Object | %{$_.Count}
+$Cdate6MonthsPlus = $files | Where-Object {$_.CreationTime -lt $date6Months } | Measure-Object | ForEach-Object{$_.Count}
 $Cdate6MonthsPlusCap = ($files | Where-Object {$_.CreationTime -lt $date6Months } | Measure-Object -Sum length).sum / 1GB
 
 
@@ -76,22 +68,24 @@ Write-Host "Older than 6 Months"("{0:P2}" -f ($Cdate6MonthsPlus / $CtotalFiles))
 #Error check
 
 $check = ($CtotalFiles - ($CdateWeek1 + $CdateMonths + $Cdate6months1 + $Cdate6MonthsPlus))
-$check2 = if ($check -eq 0) { Write-Host "Error Check: All is good" } else { Write-Host "Error Check: ERROR missing" $check "files" }
+if ($check -eq 0) { Write-Host "Error Check: All is good" } else { Write-Host "Error Check: ERROR missing" $check "files" }
+$checkCap = ($cap - ($CdateWeek1Cap + $CdateMonthsCap + $Cdate6Months1Cap + $Cdate6MonthsPlusCap))
+if ($checkCap -eq 0) { Write-Host "Cap check: All is good" } else { Write-Host "Cap Check: Error missing" $checkCap "GB" }
 
 #Get the file count for Modified in each timeframe
 
-$MtotalFiles = $files | Measure-Object | %{$_.Count}
+$MtotalFiles = $files | Measure-Object | ForEach-Object{$_.Count}
 
-$MdateWeek1 = $files | Where-Object {$_.LastWriteTime -ge $dateWeek } | Measure-Object | %{$_.Count}
+$MdateWeek1 = $files | Where-Object {$_.LastWriteTime -ge $dateWeek } | Measure-Object | ForEach-Object{$_.Count}
 $MdateWeek1Cap = ($files | Where-Object {$_.LastWriteTime -ge $dateWeek } | Measure-Object -Sum length).sum /1GB
 
-$MdateMonths = $files | Where-Object {$_.LastWriteTime -ge $dateMonth -and $_.LastWriteTime -lt $dateWeek } | Measure-Object | %{$_.Count}
+$MdateMonths = $files | Where-Object {$_.LastWriteTime -ge $dateMonth -and $_.LastWriteTime -lt $dateWeek } | Measure-Object | ForEach-Object{$_.Count}
 $MdateMonthsCap = ($files | Where-Object {$_.LastWriteTime -ge $dateMonth -and $_.CreationTime -lt $dateWeek } | Measure-Object -Sum length).sum / 1GB
 
-$Mdate6Months1 = $files | Where-Object {$_.LastWriteTime -ge $date6Months -and $_.LastWriteTime -lt $dateMonth } | Measure-Object | %{$_.Count}
+$Mdate6Months1 = $files | Where-Object {$_.LastWriteTime -ge $date6Months -and $_.LastWriteTime -lt $dateMonth } | Measure-Object | ForEach-Object{$_.Count}
 $Mdate6Months1Cap = ($files | Where-Object {$_.LastWriteTime -ge $date6Months -and $_.CreationTime -lt $dateMonth } | Measure-Object -Sum length).sum / 1GB
 
-$Mdate6MonthsPlus = $files | Where-Object {$_.LastWriteTime -lt $date6Months } | Measure-Object | %{$_.Count}
+$Mdate6MonthsPlus = $files | Where-Object {$_.LastWriteTime -lt $date6Months } | Measure-Object | ForEach-Object{$_.Count}
 $Mdate6MonthsPlusCap = ($files | Where-Object {$_.LastWriteTime -lt $date6Months } | Measure-Object -Sum length).sum / 1GB
 
 #Calculations for Modified in each timeframe
@@ -107,25 +101,26 @@ Write-Host "Older than 6 Months"("{0:P2}" -f ($Mdate6MonthsPlus / $MtotalFiles))
 #Error check
 
 $check = ($MtotalFiles - ($MdateWeek1 + $MdateMonths + $Mdate6months1 + $Mdate6MonthsPlus))
-$check2 = if ($check -eq 0) { Write-Host "Error Check: All is good" } else { Write-Host "Error Check: ERROR missing" $check "files" }
-
+if ($check -eq 0) { Write-Host "Error Check: All is good" } else { Write-Host "Error Check: ERROR missing" $check "files" }
+$checkCap = ($cap - ($MdateWeek1Cap + $MdateMonthsCap + $Mdate6Months1Cap + $Mdate6MonthsPlusCap))
+if ($checkCap -eq 0) { Write-Host "Cap check: All is good" } else { Write-Host "Cap Check: Error missing" $checkCap "GB" }
 
 
 
 #Get the file count for Last Access time in each timeframe
 
-$AtotalFiles = $files | Measure-Object | %{$_.Count}
+$AtotalFiles = $files | Measure-Object | ForEach-Object{$_.Count}
 
-$AdateWeek1 = $files | Where-Object {$_.LastAccessTime -ge $dateWeek } | Measure-Object | %{$_.Count}
+$AdateWeek1 = $files | Where-Object {$_.LastAccessTime -ge $dateWeek } | Measure-Object | ForEach-Object{$_.Count}
 $AdateWeek1Cap = ($files | Where-Object {$_.LastAccessTime -ge $dateWeek } | Measure-Object -Sum length).sum /1GB
 
-$AdateMonths = $files | Where-Object {$_.LastAccessTime -ge $dateMonth -and $_.LastAccessTime -lt $dateWeek } | Measure-Object | %{$_.Count}
+$AdateMonths = $files | Where-Object {$_.LastAccessTime -ge $dateMonth -and $_.LastAccessTime -lt $dateWeek } | Measure-Object | ForEach-Object{$_.Count}
 $AdateMonthsCap = ($files | Where-Object {$_.LastAccessTime -ge $dateMonth -and $_.CreationTime -lt $dateWeek } | Measure-Object -Sum length).sum / 1GB
 
-$Adate6Months1 = $files | Where-Object {$_.LastAccessTime -ge $date6Months -and $_.LastAccessTime -lt $dateMonth } | Measure-Object | %{$_.Count}
+$Adate6Months1 = $files | Where-Object {$_.LastAccessTime -ge $date6Months -and $_.LastAccessTime -lt $dateMonth } | Measure-Object | ForEach-Object{$_.Count}
 $Adate6Months1Cap = ($files | Where-Object {$_.LastAccessTime -ge $date6Months -and $_.CreationTime -lt $dateMonth } | Measure-Object -Sum length).sum / 1GB
 
-$Adate6MonthsPlus = $files | Where-Object {$_.LastAccessTime -lt $date6Months } | Measure-Object | %{$_.Count}
+$Adate6MonthsPlus = $files | Where-Object {$_.LastAccessTime -lt $date6Months } | Measure-Object | ForEach-Object{$_.Count}
 $Adate6MonthsPlusCap = ($files | Where-Object {$_.LastAccessTime -lt $date6Months } | Measure-Object -Sum length).sum / 1GB
 
 #Calculations for Last Access time in each timeframe
@@ -141,14 +136,14 @@ Write-Host "Older than 6 Months"("{0:P2}" -f ($Adate6MonthsPlus / $AtotalFiles))
 #Error check
 
 $check = ($AtotalFiles - ($AdateWeek1 + $AdateMonths + $Adate6months1 + $Adate6MonthsPlus))
-$check2 = if ($check -eq 0) { Write-Host "Error Check: All is good" } else { Write-Host "Error Check: ERROR missing" $check "files" }
+if ($check -eq 0) { Write-Host "Error Check: All is good" } else { Write-Host "Error Check: ERROR missing" $check "files" }
 $checkCap = ($cap - ($AdateWeek1Cap + $AdateMonthsCap + $Adate6Months1Cap + $Adate6MonthsPlusCap))
-$CheckCap2 = if ($checkCap -eq 0) { Write-Host "Cap check: All is good" } else { Write-Host "Cap Check: Error missing" $checkCap "GB" }
+if ($checkCap -eq 0) { Write-Host "Cap check: All is good" } else { Write-Host "Cap Check: Error missing" $checkCap "GB" }
 
 #File Breakdown
 Write-Host ""
 Write-Host "Quantity of files by extension"
-$files | group Extension -NoElement | sort count -desc
+$files | Group-Object Extension -NoElement | Sort-Object count -desc
 
 Write-Host "All-Done"
 Start-Sleep 100
